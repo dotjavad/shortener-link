43cb843d08248ae7f688a496142980801d62d08a
Javad <mirjavad@webtamin.com>
2019-11-06 18:09:27 +0330

    Updates

---
+++ precache-manifest.8e4cbab8f2d4975e68f74ed0cce2eb34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928b8ebbc4699c2b8b2208af725b9ea5",
+    "url": "/shortener-link/index.html"
+  },
+  {
+    "revision": "434aa2a96d66eafa335e",
+    "url": "/shortener-link/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "17f3dfeec471c006c7e1",
+    "url": "/shortener-link/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "434aa2a96d66eafa335e",
+    "url": "/shortener-link/static/js/2.d4864ce3.chunk.js"
+  },
+  {
+    "revision": "17f3dfeec471c006c7e1",
+    "url": "/shortener-link/static/js/main.71d64a3a.chunk.js"
+  },
+  {
+    "revision": "72dee45ba4a7507daa76",
+    "url": "/shortener-link/static/js/runtime-main.c10ef1c5.js"
+  }
+]);
